a2fe04557f38be334048238b49302e4047a26f85
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-20 00:35:18 +0530

    this is how your Queary parameter and HttpException used

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 app= FastAPI()
 def load_data():
@@ -25,5 +25,18 @@ def view_patient(patient_id: str = Path(..., description="id of the patient in t
     data=load_data()
     if(patient_id in data):
         return data[patient_id]
-    else:
-        return "the id is not found"
+    # else:
+    #     return "the id is not found"
+    raise HTTPException(status_code=404, detail="Patient not found")
+
+@app.get("/sort")
+def sort_patients(sort_by: str= Query(..., description="Sort on the basic of height, weight and bmi"), order: str=Query('asc',description="sort in asc or desc order")):
+    valid_fields=['height','weight','bmi']
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400,detail=f'Invalid field select from {valid_fields}')
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400,detail=f'Invalid order select between asc or desc')
+    data=load_data()
+    sort_order=True if order=='desc' else False
+    sorted_data=sorted(data.values(), key=lambda x: x.get(sort_by,0),reverse=sort_order)
+    return sorted_data
