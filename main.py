f7f626eb9ae78b9c888a993b9d61c36fc9c9509b
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-19 23:55:52 +0530

    this is method through which i can see my json data

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+import json
 app= FastAPI()
+def load_data():
+    with open("patients.json","r") as f:
+        data= json.load(f)
+    return (data)    
 
 @app.get("/")
 def hello():
-    return {"message":"hello world"}
+    return {"message":"Patient Management System API"}
 
 @app.get("/about")
 def about():
     return {"message": "we dont talk anymore like we use to do"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
