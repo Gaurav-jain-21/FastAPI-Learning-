257f950dbb80fb42165fde372fafd50a4107fc4f
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-19 23:41:10 +0530

    this is my second commit

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ app= FastAPI()
 @app.get("/")
 def hello():
     return {"message":"hello world"}
+
+@app.get("/about")
+def about():
+    return {"message": "we dont talk anymore like we use to do"}
